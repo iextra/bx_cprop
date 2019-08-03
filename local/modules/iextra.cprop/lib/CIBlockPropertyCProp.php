0b93a840db69ddc979f63cbae15638a95e0ea09b
iextra <iextra.denis@gmailcom>
2019-08-03 20:00:19 +0300

    added access modifiers

--- local/modules/iextra.cprop/lib/CIBlockPropertyCProp.php
+++ local/modules/iextra.cprop/lib/CIBlockPropertyCProp.php
@@ -4,11 +4,11 @@ use \Bitrix\Main\Localization\Loc;
 
 class CIBlockPropertyCProp
 {
-    static $arFields = [];
-    static $showedСss = false;
-    static $showedJs = false;
+    private static $arFields = [];
+    private static $showedСss = false;
+    private static $showedJs = false;
 
-    function GetUserTypeDescription()
+    public function GetUserTypeDescription()
     {
         return array(
             'PROPERTY_TYPE' => 'S',
@@ -23,62 +23,7 @@ class CIBlockPropertyCProp
         );
     }
 
-    static function showCss()
-    {
-        if(!self::$showedСss) {
-            self::$showedСss = true;
-            ?>
-            <style>
-                .cl{cursor: pointer;}
-                .mf-gray{color: #797777;}
-                .mf-fields-list{display: none; padding: 10px 0; margin-left: -300px!important;}
-                .mf-fields-list.active{display: block;}
-                .mf-fields-list td:first-child{width: 300px; color: #616060;}
-                .mf-fields-list td:last-child{padding-left: 5px;}
-                .mf-fields-list input[type="text"]{width: 350px!important;}
-            </style>
-            <?
-        }
-    }
-
-    static function showJs()
-    {
-        $showText = Loc::getMessage('IEX_CPROP_SHOW_TEXT');
-        $hideText = Loc::getMessage('IEX_CPROP_HIDE_TEXT');
-
-        CJSCore::Init(array("jquery"));
-        if(!self::$showedJs) {
-            self::$showedJs = true;
-            ?>
-            <script>
-                $(document).on('click', 'a.mf-toggle', function (e) {
-                    e.preventDefault();
-
-                    var table = $(this).closest('tr').find('table');
-                    $(table).toggleClass('active');
-                    if($(table).hasClass('active')){
-                        $(this).text('<?=$hideText?>');
-                    }
-                    else{
-                        $(this).text('<?=$showText?>');
-                    }
-                });
-
-                $(document).on('click', 'a.mf-delete', function (e) {
-                    e.preventDefault();
-
-                    var inputs = $(this).closest('tr').find('input');
-                    $(inputs).each(function (i, item) {
-                        $(item).val('');
-                    });
-                });
-            </script>
-            <?
-        }
-    }
-
-
-    function GetPropertyFieldHtml($arProperty, $value, $strHTMLControlName)
+    public function GetPropertyFieldHtml($arProperty, $value, $strHTMLControlName)
     {
         $hideText = Loc::getMessage('IEX_CPROP_HIDE_TEXT');
         $clearText = Loc::getMessage('IEX_CPROP_CLEAR_TEXT');
@@ -87,7 +32,7 @@ class CIBlockPropertyCProp
         self::showJs();
 
         if(!empty($arProperty['USER_TYPE_SETTINGS'])){
-            $arFields = $arProperty['USER_TYPE_SETTINGS'];
+            $arFields = self::prepareSetting($arProperty['USER_TYPE_SETTINGS']);
         }
         else{
             return '<span>'.Loc::getMessage('IEX_CPROP_ERROR_INCORRECT_SETTINGS').'</span>';
@@ -97,19 +42,20 @@ class CIBlockPropertyCProp
         $result = '<div class="mf-gray"><a class="cl mf-toggle">'.$hideText.'</a> | <a class="cl mf-delete">'.$clearText.'</a></div>
                     <table class="mf-fields-list active">';
 
-        foreach ($arFields as $code => $name){
-            $v = !empty($value['VALUE'][$code]) ? $value['VALUE'][$code] : '';
-            $result .= '<tr>
-                    <td align="right">'.$name.': </td>
-                    <td><input type="text" value="'.$v.'" name="'.$strHTMLControlName['VALUE'].'['.$code.']"/></td>
-                </tr>';
+
+        foreach ($arFields as $code => $arItem){
+            if($arItem['TYPE'] === 'string'){
+                $result .= self::showString($code, $arItem['TITLE'], $value, $strHTMLControlName);
+            }
+            else if($arItem['TYPE'] === 'file'){
+                $result .= self::showFile($code, $arItem['TITLE'], $value, $strHTMLControlName);
+            }
         }
 
         $result .= '</table>';
 
         return $result;
     }
-
 
     public static function GetSettingsHTML($arProperty, $strHTMLControlName, &$arPropertyFields)
     {
@@ -146,7 +92,7 @@ class CIBlockPropertyCProp
                 $result .= '
                        <tr valign="top">
                            <td><input type="text" class="inp-code" size="20" value="'.$code.'"></td>
-                           <td><input type="text" class="inp-title" size="35" name="'.$strHTMLControlName["NAME"].'['.$code.'_VALUE]" value="'.$arItem['VALUE'].'"></td>
+                           <td><input type="text" class="inp-title" size="35" name="'.$strHTMLControlName["NAME"].'['.$code.'_TITLE]" value="'.$arItem['TITLE'].'"></td>
                            <td><input type="text" class="inp-sort" size="5" name="'.$strHTMLControlName["NAME"].'['.$code.'_SORT]" value="'.$arItem['SORT'].'"></td>
                            <td>
                                 <select class="inp-type" name="'.$strHTMLControlName["NAME"].'['.$code.'_TYPE]">
@@ -161,7 +107,7 @@ class CIBlockPropertyCProp
                <tr valign="top">
                     <td><input type="text" class="inp-code" size="20"></td>
                     <td><input type="text" class="inp-title" size="35"></td>
-                    <td><input type="text" class="inp-sort" size="5"></td>
+                    <td><input type="text" class="inp-sort" size="5" value="500"></td>
                     <td>
                         <select class="inp-type"> '.self::getOptionList().'</select>                        
                     </td>
@@ -178,129 +124,29 @@ class CIBlockPropertyCProp
         return $result;
     }
 
-    static function prepareSetting($arSetting)
-    {
-        $arResult = [];
-
-        foreach ($arSetting as $key => $value){
-            if(strstr($key, '_VALUE') !== false) {
-                $code = str_replace('_VALUE', '', $key);
-                $arResult[$code]['VALUE'] = $value;
-            }
-            else if(strstr($key, '_SORT') !== false) {
-                $code = str_replace('_SORT', '', $key);
-                $arResult[$code]['SORT'] = $value;
-            }
-            else if(strstr($key, '_TYPE') !== false) {
-                $code = str_replace('_TYPE', '', $key);
-                $arResult[$code]['TYPE'] = $value;
-            }
-        }
-
-        function cmp($a, $b)
-        {
-            if ($a['SORT'] == $b['SORT']) {
-                return 0;
-            }
-            return ($a['SORT'] < $b['SORT']) ? -1 : 1;
-        }
-
-        uasort($arResult, "cmp");
-
-        return $arResult;
-    }
-
-    static function getOptionList($selected = 'string')
-    {
-        $result = '';
-        $arOption = [
-            'string' => Loc::getMessage('IEX_CPROP_FIELD_TYPE_STRING'),
-            'file' => Loc::getMessage('IEX_CPROP_FIELD_TYPE_FILE'),
-            'html' => Loc::getMessage('IEX_CPROP_FIELD_TYPE_HTML')
-        ];
-
-        foreach ($arOption as $code => $name){
-            $s = '';
-            if($code === $selected){
-                $s = 'selected';
-            }
-
-            $result .= '<option value="'.$code.'" '.$s.'>'.$name.'</option>';
-        }
-
-        return $result;
-    }
-
-    static function showJsForSetting($inputName)
-    {
-        CJSCore::Init(array("jquery"));
-        ?>
-        <script>
-            function addNewRows() {
-                $("#many-fields-table").append('' +
-                    '<tr valign="top">' +
-                    '<td><input type="text" class="inp-code" size="20"></td>' +
-                    '<td><input type="text" class="inp-title" size="35"></td>' +
-                    '<td><input type="text" class="inp-sort" size="5"></td>' +
-                    '<td><select class="inp-type"><?=self::getOptionList()?></select></td>' +
-                    '</tr>');
-            }
-
-
-            $(document).on('change', '.inp-code', function(){
-                var code = $(this).val();
-
-                if(code.length <= 0){
-                    $(this).closest('tr').find('input.inp-title').removeAttr('name');
-                    $(this).closest('tr').find('input.inp-sort').removeAttr('name');
-                    $(this).closest('tr').find('select.inp-type').removeAttr('name');
-                }
-                else{
-                    $(this).closest('tr').find('input.inp-title').attr('name', '<?=$inputName?>[' + code + '_VALUE]');
-                    $(this).closest('tr').find('input.inp-sort').attr('name', '<?=$inputName?>[' + code + '_SORT]');
-                    $(this).closest('tr').find('select.inp-type').attr('name', '<?=$inputName?>[' + code + '_TYPE]');
-                }
-            });
-
-            $(document).on('input', '.inp-sort', function(){
-                var num = $(this).val();
-                $(this).val(num.replace(/[^0-9]/gim,''));
-            });
-        </script>
-        <?
-    }
-
-    static function showCssForSetting()
-    {
-        if(!self::$showedСss) {
-            self::$showedСss = true;
-            ?>
-            <style>
-                .many-fields-table {margin: 0 auto; /*display: inline;*/}
-                .mf-setting-title td {text-align: center!important; border-bottom: unset!important;}
-                .many-fields-table td {text-align: center;}
-                .many-fields-table > input, .many-fields-table > select{width: 90%!important;}
-                .inp-sort{text-align: center;}
-                .inp-type{min-width: 125px;}
-            </style>
-            <?
-        }
-    }
-
     public static function PrepareSettings($arProperty)
     {
         $result = [];
-
         if(!empty($arProperty['USER_TYPE_SETTINGS'])){
             foreach ($arProperty['USER_TYPE_SETTINGS'] as $code => $value) {
                 $result[$code] = $value;
             }
         }
-
         return $result;
     }
 
-    function ConvertToDB($arProperty, $value)
+    public static function GetLength($arProperty, $value)
+    {
+        $result = false;
+        foreach($value['VALUE'] as $val){
+            if(!empty($val)){
+                $result = true;
+            }
+        }
+        return $result;
+    }
+
+    public function ConvertToDB($arProperty, $value)
     {
         $isEmpty = true;
 
@@ -321,7 +167,7 @@ class CIBlockPropertyCProp
         return $arResult;
     }
 
-    function ConvertFromDB($arProperty, $value)
+    public function ConvertFromDB($arProperty, $value)
     {
         $return = array();
 
@@ -336,14 +182,197 @@ class CIBlockPropertyCProp
         return $return;
     }
 
-    public static function GetLength($arProperty, $value)
+    //Internals
+
+    private static function showString($code, $title, $arValue, $strHTMLControlName)
     {
-        $result = false;
-        foreach($value['VALUE'] as $val){
-            if(!empty($val)){
-                $result = true;
+        $result = '';
+
+        $v = !empty($arValue['VALUE'][$code]) ? $arValue['VALUE'][$code] : '';
+        $result .= '<tr>
+                    <td align="right">'.$title.': </td>
+                    <td><input type="text" value="'.$v.'" name="'.$strHTMLControlName['VALUE'].'['.$code.']"/></td>
+                </tr>';
+
+        return $result;
+    }
+
+    private static function showFile($code, $arField, $arValue, $strHTMLControlName)
+    {
+        //\extra::c($arValue, $code);
+        $result = '';
+
+//        $v = !empty($arValue['VALUE'][$code]) ? $arValue['VALUE'][$code] : '';
+//        $result .= '<tr>
+//                    <td align="right">'.$arField['TITLE'].': </td>
+//                    <td><input type="text" value="'.$v.'" name="'.$strHTMLControlName['VALUE'].'['.$code.']"/></td>
+//                </tr>';
+
+        return $result;
+    }
+
+    private static function showCss()
+    {
+        if(!self::$showedСss) {
+            self::$showedСss = true;
+            ?>
+            <style>
+                .cl{cursor: pointer;}
+                .mf-gray{color: #797777;}
+                .mf-fields-list{display: none; padding: 10px 0; margin-left: -300px!important;}
+                .mf-fields-list.active{display: block;}
+                .mf-fields-list td:first-child{width: 300px; color: #616060;}
+                .mf-fields-list td:last-child{padding-left: 5px;}
+                .mf-fields-list input[type="text"]{width: 350px!important;}
+            </style>
+            <?
+        }
+    }
+
+    private static function showJs()
+    {
+        $showText = Loc::getMessage('IEX_CPROP_SHOW_TEXT');
+        $hideText = Loc::getMessage('IEX_CPROP_HIDE_TEXT');
+
+        CJSCore::Init(array("jquery"));
+        if(!self::$showedJs) {
+            self::$showedJs = true;
+            ?>
+            <script>
+                $(document).on('click', 'a.mf-toggle', function (e) {
+                    e.preventDefault();
+
+                    var table = $(this).closest('tr').find('table');
+                    $(table).toggleClass('active');
+                    if($(table).hasClass('active')){
+                        $(this).text('<?=$hideText?>');
+                    }
+                    else{
+                        $(this).text('<?=$showText?>');
+                    }
+                });
+
+                $(document).on('click', 'a.mf-delete', function (e) {
+                    e.preventDefault();
+
+                    var inputs = $(this).closest('tr').find('input');
+                    $(inputs).each(function (i, item) {
+                        $(item).val('');
+                    });
+                });
+            </script>
+            <?
+        }
+    }
+
+    private static function showJsForSetting($inputName)
+    {
+        CJSCore::Init(array("jquery"));
+        ?>
+        <script>
+            function addNewRows() {
+                $("#many-fields-table").append('' +
+                    '<tr valign="top">' +
+                    '<td><input type="text" class="inp-code" size="20"></td>' +
+                    '<td><input type="text" class="inp-title" size="35"></td>' +
+                    '<td><input type="text" class="inp-sort" size="5" value="500"></td>' +
+                    '<td><select class="inp-type"><?=self::getOptionList()?></select></td>' +
+                    '</tr>');
+            }
+
+
+            $(document).on('change', '.inp-code', function(){
+                var code = $(this).val();
+
+                if(code.length <= 0){
+                    $(this).closest('tr').find('input.inp-title').removeAttr('name');
+                    $(this).closest('tr').find('input.inp-sort').removeAttr('name');
+                    $(this).closest('tr').find('select.inp-type').removeAttr('name');
+                }
+                else{
+                    $(this).closest('tr').find('input.inp-title').attr('name', '<?=$inputName?>[' + code + '_TITLE]');
+                    $(this).closest('tr').find('input.inp-sort').attr('name', '<?=$inputName?>[' + code + '_SORT]');
+                    $(this).closest('tr').find('select.inp-type').attr('name', '<?=$inputName?>[' + code + '_TYPE]');
+                }
+            });
+
+            $(document).on('input', '.inp-sort', function(){
+                var num = $(this).val();
+                $(this).val(num.replace(/[^0-9]/gim,''));
+            });
+        </script>
+        <?
+    }
+
+    private static function showCssForSetting()
+    {
+        if(!self::$showedСss) {
+            self::$showedСss = true;
+            ?>
+            <style>
+                .many-fields-table {margin: 0 auto; /*display: inline;*/}
+                .mf-setting-title td {text-align: center!important; border-bottom: unset!important;}
+                .many-fields-table td {text-align: center;}
+                .many-fields-table > input, .many-fields-table > select{width: 90%!important;}
+                .inp-sort{text-align: center;}
+                .inp-type{min-width: 125px;}
+            </style>
+            <?
+        }
+    }
+
+    private static function prepareSetting($arSetting)
+    {
+        $arResult = [];
+
+        foreach ($arSetting as $key => $value){
+            if(strstr($key, '_TITLE') !== false) {
+                $code = str_replace('_TITLE', '', $key);
+                $arResult[$code]['TITLE'] = $value;
+            }
+            else if(strstr($key, '_SORT') !== false) {
+                $code = str_replace('_SORT', '', $key);
+                $arResult[$code]['SORT'] = $value;
+            }
+            else if(strstr($key, '_TYPE') !== false) {
+                $code = str_replace('_TYPE', '', $key);
+                $arResult[$code]['TYPE'] = $value;
             }
         }
+
+        if(!function_exists('cmp')){
+            function cmp($a, $b)
+            {
+                if ($a['SORT'] == $b['SORT']) {
+                    return 0;
+                }
+                return ($a['SORT'] < $b['SORT']) ? -1 : 1;
+            }
+        }
+
+        uasort($arResult, 'cmp');
+
+        return $arResult;
+    }
+
+    private static function getOptionList($selected = 'string')
+    {
+        $result = '';
+        $arOption = [
+            'string' => Loc::getMessage('IEX_CPROP_FIELD_TYPE_STRING'),
+            'file' => Loc::getMessage('IEX_CPROP_FIELD_TYPE_FILE'),
+            'html' => Loc::getMessage('IEX_CPROP_FIELD_TYPE_HTML')
+        ];
+
+        foreach ($arOption as $code => $name){
+            $s = '';
+            if($code === $selected){
+                $s = 'selected';
+            }
+
+            $result .= '<option value="'.$code.'" '.$s.'>'.$name.'</option>';
+        }
+
         return $result;
     }
 }
